8afa6713481f4fa623ca0cb13544089fa9ae8db8
Marwane Ezzaze <marwane.ezzaze@omnevo.net>
2022-08-10 23:16:09 +0100

    Cleaning unit tests

--- tests/ExceptionTest.php
+++ tests/ExceptionTest.php
@@ -6,21 +6,21 @@ use Illuminate\Support\Facades\Config;
 
 it('can throw invalid API key exception', function () {
     Config::set('airlabs.version', 'v9');
-    Config::set('airlabs.api_key', 'ea355cf5-7ed9-4d57-9e75-acf544ee2deee');
+    Config::set('airlabs.api_key', '');
     Config::set('airlabs.cache.enabled', true);
     $result = Airlabs::verifyPeer(false)->ping();
 })->throws(AirlabsException::class, 'Provided API Key is invalid.');
 
 it('can throw not supported method exception', function () {
     Config::set('airlabs.version', 'v9');
-    Config::set('airlabs.api_key', 'ea355cf5-7ed9-4d57-9e75-acf544ee2dee');
+    Config::set('airlabs.api_key', '');
     Config::set('airlabs.cache.enabled', true);
     $result = Airlabs::verifyPeer(false)->unkownMethod();
 })->throws(AirlabsException::class, 'Provided method is not supported.');
 
 it('can throw wrong parameters exception', function () {
     Config::set('airlabs.version', 'v9');
-    Config::set('airlabs.api_key', 'ea355cf5-7ed9-4d57-9e75-acf544ee2dee');
+    Config::set('airlabs.api_key', '');
     Config::set('airlabs.cache.enabled', true);
     $result = Airlabs::verifyPeer(false)->schedules();
 })->throws(AirlabsException::class, 'Some parameters are wrong.');
--- tests/InitTest.php
+++ tests/InitTest.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Config;
 
 it('can ping remote api', function () {
     Config::set('airlabs.version', 'v9');
-    Config::set('airlabs.api_key', 'ea355cf5-7ed9-4d57-9e75-acf544ee2dee');
+    Config::set('airlabs.api_key', '');
     Config::set('airlabs.cache.enabled', true);
     $result = Airlabs::verifyPeer(false)->ping();
     expect($result)->toBeTrue();
